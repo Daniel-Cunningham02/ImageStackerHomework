f3fda566dd0f0c370fcf65b5821640a130f27fd6
Anthony Osborne <antrosbo@ut.utm.edu>
2023-03-23 11:08:52 -0500

    testing push ability

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file main.cpp
+ * @author Anthony Osborne, Daniel Cunningham, 
+ * @date 2023-03-23
+ * @brief driver code 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Stacker.h"
+using namespace std;
+
+int main() {
+  string image;
+  int numStack;
+  cout <<"Please enter the image you wish to stack: " <<;
+  cin >> image;
+  cout << endl;
+  cout <<"Please enter the number of images: " <<;
+  cin >> numStack;
+  cout << endl;
+  
+  return 0;
+}
